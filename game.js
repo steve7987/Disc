cbc61f4846216241c6963e44ba2d831d7300009c
Stephen Russell <steve.7987@gmail.com>
2015-04-14 23:49:16 +0900

    Added Remaining States
    Added the ready, action, score, turn , turnact and turnscore states.

--- game.js
+++ game.js
@@ -86,21 +86,145 @@ function onEnterPeriod(from, variables){
 	variables.ctx.fillText(variables.gScore + " to " + variables.bScore, variables.canvas.width / 2, 150);
 	
 	variables.yards = 15;
+	variables.player = "Steve";  //current player with the disc
 }
 
 function onClickPeriod(x, y, fsm, variables){
-	fsm.change("EndGame");
+	fsm.change("Ready");
 }
+
+//ready functions
+function onEnterReady(from, variables){
+	resetCanvas(variables);
+	variables.ctx.fillStyle = "#0f0f0f";
+	variables.ctx.textAlign = "center";
+	variables.ctx.fillText(variables.player + " has the disc.", variables.canvas.width / 2, 100);
+	variables.optionBox = new OptionBox(actionList, {x: 100, y: 400, align: "left"});
+	variables.optionBox.display(variables.ctx);
+}
+
+function onClickReady(x, y, fsm, variables){
+	var clickedOn = variables.optionBox.getFromClick(x, y);
+	if (clickedOn != -1){
+		variables.action = clickedOn;
+		fsm.change("Action");
+	}
+}
+
+function onMoveReady(x, y, fsm, variables){
+	variables.optionBox.highlight(x, y, variables.ctx);
+}
+
+//action functions
+function onEnterAction(from, variables){
+	resetCanvas(variables);
+	variables.ctx.fillStyle = "#0f0f0f";
+	variables.ctx.textAlign = "center";
+	variables.ctx.fillText(variables.player + " uses " + actionList[variables.action], variables.canvas.width / 2, 100);
+	if (Math.random() > 0.4){
+		variables.ctx.fillText("Its super effective", variables.canvas.width / 2, 500);
+		variables.yards += 50;
+		variables.success = true;
+	}
+	else {
+		variables.ctx.fillText("Its not very effective", variables.canvas.width / 2, 500);
+		variables.success = false;
+	}
+}
+
+function onClickAction(x, y, fsm, variables){
+	if (variables.success && variables.yards > 100){
+		fsm.change("Score");
+	}
+	else if (variables.success){
+		fsm.change("Ready");
+	}
+	else {
+		fsm.change("Turn");
+	}
+}
+
+//score functions
+function onEnterScore(from, variables){
+	resetCanvas(variables);
+	variables.ctx.fillStyle = "#0f0f0f";
+	variables.ctx.textAlign = "center";
+	variables.ctx.fillText(variables.team + " scores!!!", variables.canvas.width / 2, 100);
+	variables.gScore++;
+	variables.ctx.fillText(variables.gScore + " to " + variables.bScore, variables.canvas.width / 2, 125);
+}
+
+function onClickScore(x, y, fsm, variables){
+	if (variables.gScore < 2){
+		variables.period++;
+		fsm.change("Period");
+	}
+	else {
+		fsm.change("EndGame");
+	}
+}
+
+//turn functions
+function onEnterTurn(from, variables){
+	resetCanvas(variables);
+	variables.ctx.fillStyle = "#0f0f0f";
+	variables.ctx.textAlign = "center";
+	variables.ctx.fillText(variables.opponent + " gets the disc.", variables.canvas.width / 2, 100);
+}
+
+function onClickTurn(x, y, fsm, variables){
+	fsm.change("TurnAct");
+}
+
+//turn act functions
+function onEnterTurnAct(from, variables){
+	resetCanvas(variables);
+	variables.ctx.fillStyle = "#0f0f0f";
+	variables.ctx.textAlign = "center";
+	variables.ctx.fillText("They huck it deep downfield...", variables.canvas.width / 2, 100);
+}
+
+function onClickTurnAct(x, y, fsm, variables){
+	fsm.change("TurnScore");
+}
+
+
+//turn score functions
+function onEnterTurnScore(from, variables){
+	resetCanvas(variables);
+	variables.ctx.fillStyle = "#0f0f0f";
+	variables.ctx.textAlign = "center";
+	variables.ctx.fillText("They huck it deep downfield...", variables.canvas.width / 2, 100);
+	variables.ctx.fillText("... and score!!!", variables.canvas.width / 2, 500);
+	variables.bScore++;
+}
+
+function onClickTurnScore(x, y, fsm, variables){
+	if (variables.bScore < 2){
+		variables.period++;
+		fsm.change("Period");
+	}
+	else {
+		fsm.change("EndGame");
+	}
+}
+
 
 //end game functions
 function onEnterEndGame(from, variables){
 	resetCanvas(variables);
 	variables.ctx.fillStyle = "#0f0f0f";
-	variables.ctx.fillText(variables.team + " wins", variables.canvas.width / 2, 100);
+	if (variables.gScore > variables.bScore){
+		variables.ctx.fillText(variables.team + " wins", variables.canvas.width / 2, 100);
+	}
+	else {
+		variables.ctx.fillText(variables.opponent + " wins", variables.canvas.width / 2, 100);
+	}
+	variables.ctx.fillText("Click to play again...", variables.canvas.width / 2, 500);
 }
 
 function onClickEndGame(x, y, fsm, variables){
-	fsm.change("Menu");
+	fsm.change("TSelect");
 }
 
 function Start() {
@@ -119,6 +243,25 @@ function Start() {
 			
 			onenterPeriod: function(from, variables) { onEnterPeriod(from, variables) },
 			onclickPeriod: function(x, y, fsm, variables) { onClickPeriod(x, y, fsm, variables) },
+			
+			onenterReady: function(from, variables) {onEnterReady(from, variables) },
+			onclickReady: function(x, y, fsm, variables) {onClickReady(x, y, fsm, variables); },
+			onmoveReady: function(x, y, fsm, variables) {onMoveReady(x, y, fsm, variables); },
+			
+			onenterAction: function(from, variables) { onEnterAction(from, variables) },
+			onclickAction: function(x, y, fsm, variables) { onClickAction(x, y, fsm, variables) },
+			
+			onenterScore: function(from, variables) { onEnterScore(from, variables) },
+			onclickScore: function(x, y, fsm, variables) { onClickScore(x, y, fsm, variables) },
+			
+			onenterTurn: function(from, variables) { onEnterTurn(from, variables) },
+			onclickTurn: function(x, y, fsm, variables) { onClickTurn(x, y, fsm, variables) },
+			
+			onenterTurnAct: function(from, variables) { onEnterTurnAct(from, variables) },
+			onclickTurnAct: function(x, y, fsm, variables) { onClickTurnAct(x, y, fsm, variables) },
+			
+			onenterTurnScore: function(from, variables) { onEnterTurnScore(from, variables) },
+			onclickTurnScore: function(x, y, fsm, variables) { onClickTurnScore(x, y, fsm, variables) },
 			
 			onenterEndGame: function(from, variables) { onEnterEndGame(from, variables) },
 			onclickEndGame: function(x, y, fsm, variables) { onClickEndGame(x, y, fsm, variables) }
--- teamdata.js
+++ teamdata.js
@@ -1,2 +1,3 @@
 //teamdata.js
 var teamList = ["Cheonan Cheonwon", "Seoul Train", "Suwon Love"];
+var actionList = ["Throw a flick", "Dump pass", "Throw a hammer"];
